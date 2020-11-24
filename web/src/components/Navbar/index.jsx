487e1896d92420e98fdb5b2a25aa8fc67cb52bfa
Eduardo ZF <eduardozf1@gmail.com>
2020-11-24 18:53:12 -0300

    🗃 Starting navbar design

---
+++ web/src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './styles';
+
+function Navbar() {
+  return (
+    <div className="Container">
+      <div className="topButtons">
+        <button className="smallBtn"></button>
+        <button className="smallBtn"></button>
+        <button className="smallBtn"></button>
+      </div>
+
+      <div className="bottomButtons">
+        <button className="smallBtn"></button>
+        <button className="smallBtn"></button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
